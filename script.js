3776d2b2e318c650907064935cb9561ed02397f0
Sonu34668 <130197661+Sonu34668@users.noreply.github.com>
2023-07-01 22:09:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,14 @@
-// function volume_sphere() {
-//     //Write your code here
-  
-// } 
-
-// Get the radius input, the volume input, and the calculate button
-const radiusInput = document.getElementById("radius");
-const volumeInput = document.getElementById("volume");
-const calculate = document.getElementById("submit");
-
-// Define the sphere volume function
 function volume_sphere() {
-  let volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
-  return volume;
-}
+    //Write your code here
+    let radius = document.getElementById("radius").value;
+   let volume =  (4/3) * Math.PI * Math.pow(radius, 3);
+	volume = volume.toFixed(4);
+     document.getElementById("volume").value = volume
+    // console.log(radius);
 
-// Add event listener to the calculate button
-// submit.addEventListener("click", () => {
-//   let radius = parseFloat(radius.value);
-//   let volumeInput = volume_sphere(radius);
-//   volume.value = volumeInput.toFixed(2);
-// });
- window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+    return false;
+     
+    
+} 
+
+window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
